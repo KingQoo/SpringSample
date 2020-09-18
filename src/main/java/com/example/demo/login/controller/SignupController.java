9c09008f62c528b1c8a6a7e74d65c34c7285a797
hiram <hiram@LAPTOP-91U35SK8>
2020-09-19 01:00:13 +0900

    UserDetail_fin

--- src/main/java/com/example/demo/login/controller/SignupController.java
+++ src/main/java/com/example/demo/login/controller/SignupController.java
@@ -3,6 +3,7 @@ package com.example.demo.login.controller;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -13,9 +14,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.login.domain.GroupOrder;
 import com.example.demo.login.domain.SignupForm;
+import com.example.demo.login.domain.model.User;
+import com.example.demo.login.domain.service.UserService;
 
 @Controller
 public class SignupController {
+	
+	@Autowired
+	UserService userService;
 
 	private Map<String, String> radioMarriage;
 	private Map<String, String> initRadioMarriage(){
@@ -43,6 +49,24 @@ public class SignupController {
 		if(bindingResult.hasErrors()) {
 			return getSignUp(form,model);
 
+		}
+		
+		User user = new User();
+		
+		user.setUserId(form.getUserId());
+		user.setPassword(form.getPassword());
+		user.setUserName(form.getUserName());
+		user.setBirthday(form.getBirthday());
+		user.setAge(form.getAge());
+		user.setMarriage(form.isMarriage());
+		user.setRole("ROLE_GENERAL");
+		
+		boolean result = userService.insert(user);
+		
+		if(result == true) {
+			System.out.println("insert成功");
+		}else {
+			System.out.println("insert失敗");
 		}
 
 		return "redirect:/login";
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,3 +4,14 @@ employee(
 	employee_name varchar(50),
 	age int
 );
+
+create table if not exists
+m_user(
+	user_id VARCHAR(50) PRIMARY KEY,
+	password VARCHAR(100),
+	user_name VARCHAR(50),
+	birthday DATE,
+	age INT,
+	marriage BOOLEAN,
+	role VARCHAR(50)
+);
