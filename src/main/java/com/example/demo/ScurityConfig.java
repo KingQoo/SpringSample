95fd81c3eb44dab573f3a571556fe98021c46d94
hiram <hiram@LAPTOP-91U35SK8>
2020-09-29 08:07:18 +0900

    start_mybatis

--- src/main/java/com/example/demo/ScurityConfig.java
+++ src/main/java/com/example/demo/ScurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 
 @EnableWebSecurity
 @Configuration
@@ -56,7 +57,7 @@ public class ScurityConfig extends WebSecurityConfigurerAdapter{
 		.antMatchers("/css/**").permitAll()
 		.antMatchers("/login").permitAll()
 		.antMatchers("/signup").permitAll()
-
+		.antMatchers("/rest/**").permitAll()
 		.antMatchers("/admin").hasAuthority("ROLE_ADMIN")
 		.anyRequest().authenticated();
 
@@ -75,7 +76,9 @@ public class ScurityConfig extends WebSecurityConfigurerAdapter{
 				.logoutUrl("/logout")
 				.logoutSuccessUrl("/login");
 
-//		http.csrf().disable();
+		RequestMatcher csrfMatcher = new RestMatcher("/rest/**");
+
+		http.csrf().requireCsrfProtectionMatcher(csrfMatcher);
 	}
 
 	@Override
---
+++ src/test/resources/testdata.sql
@@ -0,0 +1,2 @@
+INSERT INTO m_user(user_id,password,user_name,birthday,age,marriage,role)
+VALUES('satou@xxx.co.jp','$2a$10$xPu80cnnqjwUNnsR2fyev.j2S7FqJQq7U80MUri6CRkJIcncKjwYq','佐藤太郎','1990-01-01','28','false','ROLE_ADMIN');
